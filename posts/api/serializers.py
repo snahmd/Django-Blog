2e6424cd223fa15d71dfdf78982d627de5c7cd97
snahmd <sanahmed7@gmail.com>
2020-01-10 23:45:11 +0100

    serializer-rest added

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from posts.models import Post,Comment
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Comment
+        fields = ('user', 'content', 'created',)
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    class Meta:
+          model = Post
+          fields = ('url', 'title', 'user', 'created', 'comments' )
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .serializers import UserSerializer
+from .serializers import UserSerializer, PostSerializer
+from posts.models import Post
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer 
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer   
